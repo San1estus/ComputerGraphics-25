c90a081850455399f031930bbeb4d3f3dcfe8c97
San1estus <j.rodriguezisiordia@gmail.com>
2025-11-22 16:22:07 -0600

    Dispersion de Rayos tras rebotar

--- RayCastProject/RayCast.cpp
+++ RayCastProject/RayCast.cpp
@@ -1,76 +1,21 @@
-#include <iostream>
-#include "ray.hpp"
-#include "color.hpp"
-
-double hit_sphere(const point3& center, double radius, const ray& r){
-	vec3 oc = center - r.origin();
-
-	auto a = r.direction().length_squared();
-	auto h = dot(r.direction(), oc);
-	auto c = oc.length_squared() - radius*radius;
-
-	auto discriminant = h*h-a*c;
-
-	if(discriminant < 0){
-		return -1.0;
-	} else{
-		return (h - std::sqrt(discriminant)) / a;
-	}
-}
-
-color ray_color(const ray& r){
-	auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-	if(t > 0){
-		vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
-		return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
-	}
-
-	vec3 unit_direction = unit_vector(r.direction());
-	auto a = 0.5*(unit_direction.y() + 1.0);
-  return (1.0 - a)*color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
-}
+#include "rtweekend.h"
+#include "camera.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 int main(){
-  // Dimensiones de imagen
-  auto aspect_ratio = 16.0/9.0;
-  int image_width = 900;
-  int image_height = int(image_width/aspect_ratio);
-  // Verificacion de la altura
-  image_height = (image_height < 1) ? 1 : image_height;
+hittable_list world;
 
-  // Camara
-  auto focal_length = 1.0;
-  auto viewport_height = 2.0;
-  auto viewport_width = viewport_height * (double(image_width)/image_height);
-  auto camera_center = point3(0,0,0);
+  world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+  world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
-  // Calcular los vectores a lo largo de la imagen
-  auto viewport_u = vec3(viewport_width, 0, 0);     // horizontal
-  auto viewport_v = vec3(0, -viewport_height, 0);   // vertical
+  camera cam;
 
-  // Calcular la distancia entre pixeles
-  auto pixel_delta_u = viewport_u / image_width;   // horizontal
-  auto pixel_delta_v = viewport_v / image_height;  // vertical
+  cam.aspect_ratio = 16.0 / 9.0;
+  cam.image_width  = 900;
+	cam.samples_per_pixel = 100;
+	cam.max_depth = 50;
 
-	// Calcular la ubicacion del pixel en la esquina superior izquierda
-  auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
-  auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-	int cnt = 0;
-  for(int j = 0; j < image_height; j++){
-		if(double(j*100/(image_height-1)) >= cnt){
-    	std::clog << "\rImagen generada: "<< cnt << '%' << ' ' << std::flush; 
-			cnt += 10;
-		}
-    for(int i = 0; i < image_width; i++){
-			auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);		
-			auto ray_direction = pixel_center - camera_center;
-			ray r(camera_center, ray_direction);
-      color pixel_color = ray_color(r);
-      write_color(std::cout, pixel_color);
-    }
-  }
-
-  std::clog << "\rDone.                                          \n";
+  cam.render(world);
 }
---
+++ RayCastProject/rtweekend.h
@@ -0,0 +1,44 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <random>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+
+// C++ Std Usings
+
+using std::make_shared;
+using std::shared_ptr;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = acos(-1);
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max){
+	return min + (max-min)*random_double();
+}
+
+// Common Headers
+
+#include "color.h"
+#include "interval.h"
+#include "ray.h"
+#include "vec3.h"
+
+#endif
